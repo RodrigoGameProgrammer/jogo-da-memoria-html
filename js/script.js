27fc102c7fcc5c405f12146f85a37cccfc138147
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-03-05 16:48:46 -0300

    Jogo da Memória

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@
 			//mudando o css da class card, com um if inline ?(SESIM) :(SENAO)
 			card.style.left = i % 8 === 0 ? 5 + "px" : i % 8 * 165 + 5 + "px"; 
 			card.style.top = i < 8 ? 5 + "px" : 250 + "px";
+
+			card.addEventListener("click",flipCard,false);
 		}
+	}
+
+	function flipCard(){
+		//toggle vai adicionar a classe flipped se não existir e retirar ela causo exista
+		var faces = this.getElementsByClassName("face");
+		faces[0].classList.toggle("flipped");
+		faces[1].classList.toggle("flipped");
+		console.log(faces[0]);
 	}
 }());
